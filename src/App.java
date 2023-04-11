65b755ec84c524564725f4c712a66cc7d6bfbc99
Patrick Azrael <azraelpatrick356@gmail.com>
2023-04-10 21:55:59 -0300

    Primeiro Commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        int num1;
+        num1 = leia.nextInt();
+
+        System.out.println("Informe outro número: ");
+        int num2;
+        num2 = leia.nextInt();
+
+        if (num1 > num2) {
+            System.out.printf("O maior número informadoo foi [%d]", num1);
+        } else {
+            System.out.printf("O maior número informado foi [%d]", num2);
+        }
+
+    }
+}
